145d30c1d79b4c447956be676cd298397293f570
TonyYang9527 <leetony554@gmail.com>
2018-03-27 01:55:55 +0800

    modal Upload component adjust

--- src/components/upload/ModalUpload.js
+++ src/components/upload/ModalUpload.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import {observer} from "mobx-react";
-import {Icon,Upload,Modal} from 'antd';
+import { Icon, Upload, Modal, message} from 'antd';
 import {observable, action} from 'mobx';
 
 const state = observable({
     modalShow:false ,
     modalpreviewShow: false,
-    fileList: [{
+    defaultFileList: [{
         uid: -1,
         name: 'xxx.png',
         status: 'done',
@@ -28,19 +28,28 @@ const actions = {
         state.modalpreviewShow = false;
     }),
     handleChange: action((info) => {
-        console.log(JSON.stringify(info.fileList)) ;
-       // state.fileList =flist ;
-       // state.length = state.fileList.length ;
+        console.log(JSON.stringify(info.file)) ;
+        state.defaultFileList = info.fileList ;
+        if (info.file.status === 'uploading') {
+            console.log(info.file);
+        }
+        if (info.file.status === 'done') {
+            message.success(`${info.file.name} file uploaded successfully`);
+            console.log(info.file);
+        } else if (info.file.status === 'error') {
+            message.error(`${info.file.name} file upload failed.`);
+            console.log(info.file);
+        }
     }),
     handlePreview : action((file) => {
         state.previewImage=file.url || file.thumbUrl;
         state.modalpreviewShow= true;
     }),
     handleRemove: action((file) => {
-        const index = state.fileList.indexOf(file);
+        const index = state.defaultFileList.indexOf(file);
         const newFileList = state.fileList.slice();
         newFileList.splice(index, 1);
-        state.fileList= newFileList;
+        state.defaultFileList= newFileList;
     }),
 
 };
@@ -65,13 +74,13 @@ const ModalUpload = observer(({store,action}) => {
         <Upload 
            action="//jsonplaceholder.typicode.com/posts/"
            listType="picture-card"
-           fileList={state.fileList}
+           defaultFileList={state.defaultFileList}
            onPreview={action.handlePreview}
            onChange={action.handleChange}
            onRemove={action.handleRemove}
-           multiple={true}
+           multiple={false}
           >
-          { state.fileList.length >= 6? null : uploadButton}
+                {state.defaultFileList.length >= 6? null : uploadButton}
         </Upload>
             <Modal visible={state.modalpreviewShow} footer={null} onCancel={action.handlePreviewCancel}>
                 <img alt="example" style={{ width: '100%' }} src={state.previewImage} />
